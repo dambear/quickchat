361e2287596be704f6547d2849f5f0df1ce09a42
dambear <denverasi01@gmail.com>
2024-09-18 17:07:28 +0800

    configure clerk auth, sign-in, sign-up

---
+++ src/app/(routes)/chat/page.tsx
@@ -0,0 +1,15 @@
+export default function HomePage() {
+  return (
+    <>
+      <div className="flex min-h-screen w-screen gap-4">
+        <div className="w-1/4 bg-red-500"></div>
+
+        <div className="w-2/4 bg-red-500"></div>
+
+        <div className="w-1/4 bg-red-500">
+          <div className="h-14 bg-black"></div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "~/styles/globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
@@ -13,8 +15,14 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>
+          <main className="flex min-h-screen flex-col items-center justify-center">
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
