e5a2e3a3200d929db18187369c8ffe489d689f9d
dambear <denverasi01@gmail.com>
2024-09-22 17:22:19 +0800

    fix route

--- src/app/api/[...routes]/route.ts
+++ src/app/api/[...routes]/route.ts
@@ -1,11 +1,14 @@
 import { handle } from "hono/vercel";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { swaggerUI } from "@hono/swagger-ui";
-import { getAllUsersRoute, getUserRoute } from "../_routes/user/route";
+
 import {
   getAllUsers_Handler,
   getUserById_Handler,
 } from "../_handlers/user/handler";
+
+import { createRoute } from "@hono/zod-openapi";
+import { zod_userParamSchema, zod_userSchema } from "prisma/lib/zodSchema/user";
 
 const app = new OpenAPIHono();
 
@@ -20,6 +23,51 @@ app.doc("/api/doc", {
 
 // Define Swagger UI endpoint
 app.get("/api/ui", swaggerUI({ url: "/api/doc" }));
+
+// Route for getting a user by ID
+export const getUserRoute = createRoute({
+  method: "get",
+  path: "/api/users/[id]",
+  request: {
+    params: zod_userParamSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: zod_userSchema,
+        },
+      },
+      description: "Retrieve the user",
+    },
+    404: {
+      description: "User not found",
+    },
+    500: {
+      description: "Error retrieving user",
+    },
+  },
+  tags: ["User"],
+});
+
+export const getAllUsersRoute = createRoute({
+  method: "get",
+  path: "api/users",
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: zod_userSchema,
+        },
+      },
+      description: "Retrieve all user records",
+    },
+    500: {
+      description: "Error retrieving users",
+    },
+  },
+  tags: ["User"],
+});
 
 // Register the route with the handler
 app.openapi(getUserRoute, getUserById_Handler);
